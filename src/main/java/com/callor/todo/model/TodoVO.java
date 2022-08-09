38289af1f7b7aa647522611aaf9c9ab617d0d113
lee9805 <dlthwjd1161@naver.com>
2022-08-09 15:44:54 +0900

    security project

---
+++ src/main/java/com/callor/todo/model/TodoVO.java
@@ -0,0 +1,29 @@
+package com.callor.todo.model;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TodoVO {
+	
+	private long t_seq;
+	private String t_username;
+	private String t_sdate;
+	private String t_stime;
+	private String t_content;
+	private String t_edate;
+	private String t_etime;
+	
+
+}
